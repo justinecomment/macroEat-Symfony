db3abd9a5c7ca7ecd1d22a1115e5e0265e84de77
Justine <justinecomment@gmail.com>
2018-01-07 13:33:58 +0100

    + animation JS on each categories.

--- src/MacroBundle/Resources/public/js/nutriments.js
+++
@@ -1,9 +0,0 @@
-$( ".categories ul li" ).hover(
-  function() {
-      $('.name').css({transform: 'translateX(10px)', transitionDuration: '0.2s' })
-     
-  },
-  function(){
-      $('.name').css({transform: 'translateX(-10px)', transitionDuration: '0.2s'})
-  }
-);
--- web/bundles/macro/js/nutriments.js
+++ web/bundles/macro/js/nutriments.js
@@ -1,9 +1,9 @@
-$( ".categories ul li" ).hover(
-    function() {
-        $('.name').css({transform: 'translateX(10px)', transitionDuration: '0.2s' })
-       
-    },
-    function(){
-        $('.name').css({transform: 'translateX(-10px)', transitionDuration: '0.2s'})
-    }
-  );
+$( ".categories ul li" ).on({
+    mouseover: function () {
+          $(this).find('.name').css({transform: 'translateX(10px)', transitionDuration: '0.2s' })
+         
+      },
+      mouseleave: function () {
+        $(this).find('.name').css({transform: 'translateX(-10px)', transitionDuration: '0.2s'})
+      }
+ } );
